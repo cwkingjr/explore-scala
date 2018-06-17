dffbcde2d61c4e1c15e41df3c53acd932dd0384e
Chuck King <cwkingjr@gmail.com>
2018-06-17 10:29:40 -0500

    cleanup pattern matching

--- PatternMatching.scala
+++ PatternMatching.scala
@@ -1,11 +1,13 @@
 object PatternMatching {
-   def main(args: Array[String]) {
-      println(matchTest(3))
-   }
+    def main(args: Array[String]) {
+        println(matchTest(1))
+        println(matchTest(2))
+        println(matchTest(3))
+    }
    
-   def matchTest(x: Int): String = x match {
-      case 1 => "one"
-      case 2 => "two"
-      case _ => "many"
-   }
+    def matchTest(x: Int): String = x match {
+        case 1 => "one"
+        case 2 => "two"
+        case _ => "many"
+    }
 }
